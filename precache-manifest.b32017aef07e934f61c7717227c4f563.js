2370adea5642e872f50c98566c0a6ee2fe00abb8
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2019-12-11 09:27:04 -0500

    Updates

---
+++ precache-manifest.b32017aef07e934f61c7717227c4f563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86636209bb23d9c32870e0c4c4d5d9ba",
+    "url": "/indicadores/static/media/loginbg.86636209.jpg"
+  },
+  {
+    "revision": "86a0e4b5125e26b3fa72",
+    "url": "/indicadores/static/js/runtime~main.86a0e4b5.js"
+  },
+  {
+    "revision": "71a407fbb9b64d83607d",
+    "url": "/indicadores/static/js/main.71a407fb.chunk.js"
+  },
+  {
+    "revision": "9ac384b7969b8c0f2dc5",
+    "url": "/indicadores/static/js/1.9ac384b7.chunk.js"
+  },
+  {
+    "revision": "71a407fbb9b64d83607d",
+    "url": "/indicadores/static/css/main.310bcea7.chunk.css"
+  },
+  {
+    "revision": "9ac384b7969b8c0f2dc5",
+    "url": "/indicadores/static/css/1.e840941f.chunk.css"
+  },
+  {
+    "revision": "c6d3bdd35c59b3149d35e9d50d6bd43e",
+    "url": "/indicadores/index.html"
+  }
+];
